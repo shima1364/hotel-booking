97e99e999053540e49b660d160b04782352bd5ea
shima1364 <sh.movafagh.474@gmail.com>
2023-04-15 15:17:19 +0200

    10

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -28,16 +28,13 @@ export const login = async (req,res,next) =>{
    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
    if(!isPasswordCorrect) return next(createError(400, "email or password is wrong"));
    const token = jwt.sign(
-    {id: user._id, isAdmin: user.isAdmin},  process.env.Jwt)
+    {id: user._id},  process.env.Jwt)
 
    
-   const{password, isAdmin, ...otherDetails} = user._doc;
+   const{password, username, ...otherDetails} = user._doc;
    res
-   .cookie("access_token", token,{
-    httpOnly:true,
-   })
    .status(200)
-   .json({...otherDetails});
+   .json({token, ...otherDetails});
   } catch (err) {
     next(err)
   }  
